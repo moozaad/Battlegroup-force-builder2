38c409f524bd9b60cfc86914bce74d4062f77fa8
moozaad <moozaad@users.noreply.github.com>
2018-02-07 20:11:57 +0000

    Update book-template.js

--- j/book-template.js
+++ j/book-template.js
@@ -1,5 +1,7 @@
+//friendly date
 var book-date = "June 1944";
 
+//points based restrictions
 var book-armysize = [
     {points: 350, restricted-units: 2, de-ss-min: "1 Squad", de-ss-max: "1 Platoon", ca-3rd-min: "1 Squad", ca-3rd-max: "1 Platoon"},
     {points: 750, restricted-units: 3, de-ss-min: "1 Platoon", de-ss-max: "2 Platoon", ca-3rd-min: "1 Platoon", ca-3rd-max: "2 Platoon"},
@@ -8,8 +10,8 @@ var book-armysize = [
 ];
 
 
-#name - friendly name
-#var-name - used to reference other data in js (see book-armysize
+//name - friendly name
+//var-name - used to reference other data in js (see book-armysize
 var book-forces = [
     {name: "12th SS Division", var-name: "de-ss", country-flag: "de.svg", insignia-name: "de-12ss.svg"},
     {name: "Canadian 3rd Infantry Division", var-name: "ca-3rd", country-flag: "ca.svg", insignia-name: "ca-3rd-div.svg"}
