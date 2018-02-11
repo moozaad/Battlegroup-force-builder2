e548771e66e4c41b0889741329b5c8dc2bba0292
moozaad <moozaad@users.noreply.github.com>
2018-02-11 19:47:41 +0000

    Update book-template.js

--- j/book-template.js
+++ j/book-template.js
@@ -1,18 +1,18 @@
 //friendly date
-var book-date = "June 1944";
+var book_date = "June 1944";
 
 //points based restrictions
-var book-armysize = [
-    {points: 350, restricted-units: 2, de-ss-min: "1 Squad", de-ss-max: "1 Platoon", ca-3rd-min: "1 Squad", ca-3rd-max: "1 Platoon"},
-    {points: 750, restricted-units: 3, de-ss-min: "1 Platoon", de-ss-max: "2 Platoon", ca-3rd-min: "1 Platoon", ca-3rd-max: "2 Platoon"},
-    {points: 1500, restricted-units: 4, de-ss-min: "2 Platoon", de-ss-max: "3 Platoon", ca-3rd-min: "2 Platoon", ca-3rd-max: "3 Platoon"},
-    {points: 3000, restricted-units: 5, de-ss-min: "3 Platoon", de-ss-max: "6 Platoon", ca-3rd-min: "3 Platoon", ca-3rd-max: "6 Platoon"},
+var book_armysize = [
+    {points: 350, restricted_units: 2, de_ss_min: "1 Squad", de_ss_max: "1 Platoon", ca_3rd_min: "1 Squad", ca_3rd_max: "1 Platoon"},
+    {points: 750, restricted_units: 3, de_ss_min: "1 Platoon", de_ss_max: "2 Platoon", ca_3rd_min: "1 Platoon", ca_3rd_max: "2 Platoon"},
+    {points: 1500, restricted_units: 4, de_ss_min: "2 Platoon", de_ss_max: "3 Platoon", ca_3rd_min: "2 Platoon", ca_3rd_max: "3 Platoon"},
+    {points: 3000, restricted_units: 5, de_ss_min: "3 Platoon", de_ss_max: "6 Platoon", ca_3rd_min: "3 Platoon", ca_3rd_max: "6 Platoon"},
 ];
 
 
 //name - friendly name
 //var-name - used to reference other data in js (see book-armysize
-var book-forces = [
-    {name: "12th SS Division", var-name: "de-ss", country-flag: "de.svg", insignia-name: "de-12ss.svg"},
-    {name: "Canadian 3rd Infantry Division", var-name: "ca-3rd", country-flag: "ca.svg", insignia-name: "ca-3rd-div.svg"}
+var book_forces = [
+    {name: "12th SS Division", var_name: "de-ss", country_flag: "de.svg", insignia_name: "de-12ss.svg"},
+    {name: "Canadian 3rd Infantry Division", var_name: "ca-3rd", country_flag: "ca.svg", insignia_name: "ca-3rd-div.svg"}
 ];
