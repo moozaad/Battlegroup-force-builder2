83f1cbf44bd03e4b8938380c202424f1bfdab9d5
moozaad <moozaad@users.noreply.github.com>
2018-02-11 19:44:41 +0000

    Update bgb-core.js

--- j/bgb-core.js
+++ j/bgb-core.js
@@ -1,1 +1,41 @@
-# Jon Brightwell 2018-02-07
+// Jon Brightwell 2018-02-07
+
+//global stats
+var points = 0;
+var battle_rating = 0;
+var restricted_units = 0;
+var unique_units = 0;
+
+//section specific stats
+var fhq_count = 0;
+var fhq_br = 0;
+var fhq_points = 0;
+var infantry_count = 0;
+var infantry_br = 0;
+var infantry_points = 0;
+var tank_count = 0;
+var tank_br = 0;
+var tank_points = 0;
+var artillery_count = 0;
+var artillery_br = 0;
+var artillery_points = 0;
+var defence_count = 0;
+var defence_br = 0;
+var defence_points = 0;
+var recon_count = 0;
+var recon_br = 0;
+var recon_points = 0;
+var engineer_count = 0;
+var engineer_br = 0;
+var engineer_points = 0;
+var logistics_count = 0;
+var logistics_br = 0;
+var logistics_points = 0;
+var specialist_count = 0;
+var specialist_br = 0;
+var specialist_points = 0;
+var afs_count = 0;
+var afs_br = 0;
+var afs_points = 0;
+
+
